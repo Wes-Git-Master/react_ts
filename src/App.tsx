76d9d1bd70f6267b3496842605966a52d8593896
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-04-30 22:00:56 +0300

    const 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, {FC} from 'react';
 
 import './App.css';
+import {Counter} from "./components";
 
 const App: FC = () => {
   return (
-    <>
-
-    </>
+    <div className={"App"}>
+        <Counter/>
+    </div>
   );
 }
 
