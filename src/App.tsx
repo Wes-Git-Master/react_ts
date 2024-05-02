5a071cd5387d46b4b074e10d0d805b10f2039dc4
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-02 21:50:57 +0300

    lesson/2/state_lifting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React, {FC} from 'react';
-
+import React from 'react';
 import './App.css';
+import UsersComponent from "./compoennts/UsersComponent";
 
-const App: FC = () => {
-  return (
-    <>
+const App = () => {
 
-    </>
-  );
-}
+    const lift = (user: any) => {
+        console.log(user);
+    }
+
+    return (
+        <div>
+
+            <UsersComponent lift={lift}/>
+
+            <hr/>
+
+            <hr/>
+
+        </div>
+    );
+};
 
 export default App;
