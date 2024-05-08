f5ca63fc027d4b3d85e0fdb1c7631d053eee1c00
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-09 00:06:38 +0300

    additional_task/1

---
+++ src/components/User/UserComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserComponent = () => {
+    return (
+        <div>
+            UserComponent
+            
+        </div>
+    );
+};
+
+export {UserComponent};
--- src/components/Users/UsersComponent.tsx
+++ src/components/Users/UsersComponent.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {FC, useEffect, useState} from 'react';
+import {IUserPropsModel} from "../../models/UserModel/IUserModel";
+import requests from "../../services/dj.api.service";
+import {UserComponent} from "../User/UserComponent";
 
-const UsersComponent = () => {
+const UsersComponent:FC<IUserPropsModel[]> = () => {
+
+    const [users,setUsers] = useState([]);
+
+    useEffect(() => {
+        requests.users.getAllUsers().then(value => setUsers(users))
+
+    },[])
+
     return (
         <div>
-            UsersComponent
+            {
+                users.map(users=> <UserComponent/>)
+            }
             
         </div>
     );
