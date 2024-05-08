4fcdfd9b75ffa095eee89bb7b6b9f81e7af61022
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-08 19:06:38 +0300

    const/3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React, {FC} from 'react';
-
+import React, {useState} from 'react';
 import './App.css';
+import UsersComponent from "./components/users-component/UsersComponent";
+import TodosComponent from "./components/todos-component/TodosComponent";
 
-const App: FC = () => {
-  return (
-    <>
 
-    </>
-  );
-}
+const App = () => {
+    const [userId, setUserId] = useState<number>(0);
+    const choseUser = (id: number) => {
+        setUserId(id);
+    }
+
+    return (
+        <div>
+            <UsersComponent choseUser={choseUser}/>
+            {
+                !!userId && <TodosComponent userId={userId}/>
+            }
+
+        </div>
+    );
+};
 
 export default App;
