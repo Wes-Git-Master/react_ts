cde3fb4681710c35e5e5ce61620dc5ea3e4d6b3b
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-08 23:22:10 +0300

    additional_task/1

--- src/services/dj.api.service.tsx
+++ src/services/dj.api.service.tsx
@@ -2,5 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: 'https://dummyjson.com/',
-    headers: {"Content-Type" "application/json"}
+    headers: {"Content-Type" : "application/json"}
 })
+
+
