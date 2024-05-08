05faac31f05781a8140aae341386286b6d0885f6
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-08 18:52:43 +0300

    hw/3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React, {FC} from 'react';
-
+import React from 'react';
 import './App.css';
+import {useFetch} from "./custom-hooks/useFetch";
 
-const App: FC = () => {
-  return (
-    <>
 
-    </>
-  );
-}
+const App = () => {
+
+    let data = useFetch<{id:number,title:string,body:string}[]>('/posts',[]);
+    // let data = useFetch<UserModel[]>('/users', []);
+    return (
+        <div>
+            {
+                data.map((item, index) => (<div>{item.id} {item.title}</div>))}
+
+        </div>
+    );
+};
 
 export default App;
