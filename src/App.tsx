e4a6ba0bd77fd900d2c6368ba05fe9c6bceda67b
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-08 18:56:49 +0300

    lesson/3/useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import React, {FC} from 'react';
-
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-const App: FC = () => {
-  return (
-    <>
 
-    </>
-  );
-}
+const App = () => {
+
+    const [counter, setCounter] = useState<number>(0);
+    let [x, setX] = useState<number>(0);
+    useEffect(() => {
+        let id = setInterval(() => {
+            setCounter(counter + 1);
+        }, 2000);
+        return () => {
+            console.log('...useEffect return cb');
+            clearInterval(id);
+        }
+    }, [x]);
+    console.log('.');
+
+    return (
+        <div>
+            <h2>{counter}</h2>
+            <button onClick={() => {
+                setX((x) => x + 1);
+            }}></button>
+        </div>
+    );
+};
 
 export default App;
