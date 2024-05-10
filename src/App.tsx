37d1ebe02b887e550f55ab311ff8bb0d628f34ba
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-10 23:57:16 +0300

    lesson/4/high_order_component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React, {FC} from 'react';
-
+import React from 'react';
 import './App.css';
+import A from "./components/A";
+import B from "./components/B";
 
-const App: FC = () => {
-  return (
-    <>
 
-    </>
-  );
-}
+const App = () => {
+    return (
+        <div>
+
+            <A foobar={'olololo'}/>
+            <B/>
+
+        </div>
+    );
+};
 
 export default App;
