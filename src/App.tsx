71d227f6301e6a925b227a5d3c5458eabc3a6767
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-10 23:51:06 +0300

    lesson/4/hooks_performance_useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import React, {FC} from 'react';
-
+import React, {FC, memo, useCallback, useState} from 'react';
 import './App.css';
 
-const App: FC = () => {
-  return (
-    <>
+const A: FC<any> = memo(({foobar}) => {
+    console.log('A render');
+    foobar();
+    return <div>hello world</div>
+});
 
-    </>
-  );
-}
+const App = () => {
+    console.log('App render');
+    const [counter, setCounter] = useState(0);
+    const foobar = useCallback(() => {
+        //....
+    }, [counter]);
+
+
+    return (
+        <div>
+            <A foobar={foobar}/>
+
+            <button onClick={() => {
+                setCounter(counter + 1);
+            }}>click me
+            </button>
+        </div>
+    );
+};
 
 export default App;
