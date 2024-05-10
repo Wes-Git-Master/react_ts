bec6e610e079a5e06f9982fb3e74883ae7b85003
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-11 00:10:33 +0300

    const/4/part_1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React, {FC} from 'react';
-
+import React from 'react';
 import './App.css';
+import ChildComponent1 from "./components/ChildComponent1";
 
-const App: FC = () => {
-  return (
-    <>
 
-    </>
-  );
-}
+const App = () => {
+
+    const foobar = (arg1: any) => {
+        console.log(arg1);
+    }
+
+    return (
+        <div>
+            <ChildComponent1 foobar={foobar}/>
+        </div>
+    );
+};
 
 export default App;
