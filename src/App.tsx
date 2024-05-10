2ce45506a5454a93d825459d21e8af8f60b355ff
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-11 00:12:16 +0300

    const/4/part_2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import React, {FC} from 'react';
-
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import ChildComponent1 from "./components/ChildComponent1";
+import {getUsers} from "./services/axios.service";
+import {User} from "./models/User";
 
-const App: FC = () => {
-  return (
-    <>
 
-    </>
-  );
-}
+const App = () => {
+
+    const foobar = (arg1: any) => {
+        console.log(arg1);
+    }
+
+    const [users, setUsers] = useState<User[]>([])
+
+    useEffect(() => {
+        getUsers().then(value => {
+            setUsers(value.data);
+        });
+    }, []);
+
+    return (
+        <div>
+            {/*<ChildComponent1 item={100500} foobar={foobar}/>*/}
+
+            {
+                users.map(value => {
+                    return <div>{value.username}</div>
+                })
+            }
+        </div>
+    );
+};
 
 export default App;
