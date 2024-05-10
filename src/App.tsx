867754fa8fb7994d36a851728cb3f7d4d54313cb
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-10 23:53:19 +0300

    lesson/4/hooks_performance_useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import React, {FC} from 'react';
-
+import React, {FC, memo, useCallback, useMemo, useState} from 'react';
 import './App.css';
 
-const App: FC = () => {
-  return (
-    <>
+const A: FC<any> = memo(({foobar}) => {
+    console.log('A render');
+    foobar();
+    return <div>hello world</div>
+});
 
-    </>
-  );
-}
+const App = () => {
+    console.log('App render');
+    const [counter, setCounter] = useState(0);
+    const foobar = useCallback(() => {
+        //....
+    }, []);
+
+    const arr = useMemo(() => {
+        return [];
+    }, []);
+
+    return (
+        <div>
+            <A foobar={foobar} arr={arr}/>
+
+            <button onClick={() => {
+                setCounter(counter + 1);
+            }}>click me
+            </button>
+        </div>
+    );
+};
 
 export default App;
