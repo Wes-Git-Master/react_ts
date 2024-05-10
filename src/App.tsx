9f75cdff6b113221ea6a62aaed9a3942797bcf1f
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-10 23:48:26 +0300

    lesson/4/hooks_performance_memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React, {FC} from 'react';
-
+import React, {memo, useState} from 'react';
 import './App.css';
 
-const App: FC = () => {
-  return (
-    <>
+const A = memo(() => {
+    console.log('A render');
+    return <div>hello world</div>
+});
 
-    </>
-  );
-}
+const App = () => {
+    console.log('App render');
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <div>
+            <A/>
+
+            <button onClick={() => {
+                setCounter(counter + 1);
+            }}>click me
+            </button>
+        </div>
+    );
+};
 
 export default App;
