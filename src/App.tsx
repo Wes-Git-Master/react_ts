b1eefa1bcfaa146d07fcd169782735182b55cad1
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-14 14:44:06 +0300

    analysis/lesson5/forms_uncontrolled

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, {FC} from 'react';
 
 import './App.css';
+import {FormComponent} from "./components/FormComponent";
 
 const App: FC = () => {
-  return (
-    <>
 
-    </>
-  );
+    return (
+        <>
+            <div contentEditable={true}>Lorem ipsum dolor sit amet.</div>
+            <FormComponent/>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/FormComponent.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const FormComponent = () => {
+    return (
+        <div>
+
+            <form>
+
+                <input type="text" name={'username'}/>
+                <input type="text" name={'password'}/>
+
+            </form>
+
+        </div>
+    );
+};
+
+export {FormComponent};
