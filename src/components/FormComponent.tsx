7d1772c82cd5b4beaccca7cdb0eb0eb219fc2c38
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-14 16:29:59 +0300

    analysis/lesson5/forms_useForm

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import {useForm} from "react-hook-form";
 
 interface IFormProps{
     username:string,
-    password:string
+    password:string,
+    age:number
 }
 
 const FormComponent = () => {
 
+    const {handleSubmit,register} = useForm<IFormProps>();
 
+
+    const customHandler = (formDataProps:IFormProps) => {
+        console.log(formDataProps)
+    };
     return (
         <div>
-            <form>
-                <input type="text" name={'username'}/>
-                <input type="text" name={'password'}/>
+            <form onSubmit={handleSubmit(customHandler)}>
+                <input type="text" {...register('username')}/>
+                <input type="text" {...register('password')}/>
+                <input type="number" {...register('age')}/>
+                <button>send</button>
             </form>
             
         </div>
