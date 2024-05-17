a4dde1fb0d1f7590d1c97bb6ac3f0918a9ce373b
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-18 01:50:05 +0300

    hw/7_routes_params

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const ErrorPage = () => {
     return (
         <div>
-            <img src="https://t3.ftcdn.net/jpg/05/69/33/84/360_F_569338440_ncJoYhX5QR3HfB8Gx35MP5uRdi2mY1kC.jpg" alt="#"/>
+            <img src="https://t3.ftcdn.net/jpg/05/69/33/84/360_F_569338440_ncJoYhX5QR3HfB8Gx35MP5uRdi2mY1kC.jpg"
+                 alt="#"/>
         </div>
     );
 };
