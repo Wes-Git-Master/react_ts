d1a11d37ea7db860f4d2e7525036d5d6412f9779
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-18 01:49:30 +0300

    hw/6_routes

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ErrorPage = () => {
     return (
         <div>
-            <img src="https://st4.depositphotos.com/1071909/21611/i/450/depositphotos_216116470-stock-photo-http-404-error-found-page.jpg" alt="#"/>
+            <img src="https://t3.ftcdn.net/jpg/05/69/33/84/360_F_569338440_ncJoYhX5QR3HfB8Gx35MP5uRdi2mY1kC.jpg" alt="#"/>
         </div>
     );
 };
