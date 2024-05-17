1d8b6995c64ee6229367549168de1f63bf72cdb3
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-18 01:38:25 +0300

    hw/6_routes

---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <img src="https://st4.depositphotos.com/1071909/21611/i/450/depositphotos_216116470-stock-photo-http-404-error-found-page.jpg" alt="#"/>
+        </div>
+    );
+};
+
+export {ErrorPage};
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,12 +5,14 @@ import {HomePage} from "../pages/HomePage/HomePage";
 import {UsersComponent} from "../components/UsersComponent/UsersComponent";
 import {PostsComponent} from "../components/PostsComponent/PostsComponent";
 import {CommentsComponent} from "../components/CommentsComponent/CommentsComponent";
+import {ErrorPage} from "../pages/ErrorPage/ErrorPage";
 
 export const routerConfig = createBrowserRouter(
     [
         {
             path: '/',
             element: <MainLayout/>,
+            errorElement: <ErrorPage/>,
             children: [
                 {
                     index: true,
