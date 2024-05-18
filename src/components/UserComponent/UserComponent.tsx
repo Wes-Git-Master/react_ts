e0a64a1b6c1d7e1b018564e47aae2217d94bd9b2
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-05-18 23:47:00 +0300

    hw/7_routes_params

--- src/components/UserComponent/UserComponent.tsx
+++ src/components/UserComponent/UserComponent.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react';
 import {IUserModel} from "../../model/IUserModel";
 import styles from '../../styles/user.style.module.css'
+
 interface IProps {
     user: IUserModel
 }
@@ -8,9 +9,11 @@ interface IProps {
 type IPropsType = IProps & { children?: React.ReactNode }
 const UserComponent: FC<IPropsType> = ({user}) => {
     return (
-        <div className={styles.singleUserBlock}>
-            <p>id : {user.id}</p>
-            <p>Name : {user.name}</p>
+        <div>
+            <div className={styles.singleUserBlock}>
+                <p className={styles.userId}>id : {user.id}</p>
+                <p className={styles.userName}>Name : {user.name}</p>
+            </div>
         </div>
     );
 };
