e2e4b196f9fc729a96846560af954844f8efa94c
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-06-04 11:45:08 +0300

    lesson/11/state_manager_context_as_state_manager

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Outlet} from "react-router-dom";
+import HeaderComponent from "./components/HeaderComponent";
+import {postService, userService} from "./services/api.service";
+import {Context} from "./context/ContextProvider";
+import {IUserModel} from "./models/IUserModel";
+import {IPostModel} from "./models/IPostModel";
 
 
 const App = () => {
+
+    const [users, setUsers] = useState<IUserModel[]>([]);
+    const [posts, setPosts] = useState<IPostModel[]>([]);
+    const [favoriteUserState, setFavoriteUserState] = useState<IUserModel | null>(null);
+
+
+    useEffect(() => {
+        userService.getUsers().then(value => setUsers(value.data));
+        postService.getPosts().then(value => setPosts(value.data));
+    }, []);
+
+    const setFavoriteUser = (obj: IUserModel) => {
+        setFavoriteUserState(obj);
+    }
+
     return (
         <div>
-            App
+            <HeaderComponent/>
+
+            <Context.Provider value={{
+                userStore: {
+                    allUsers: users,
+                    setFavoriteUser: (obj: IUserModel) => setFavoriteUser(obj)
+                },
+                postStore: {
+                    allPosts: posts,
+                }
+            }}>
+                <Outlet/>
+            </Context.Provider>
+
+            <hr/>
+            {favoriteUserState && <div>{favoriteUserState.email}</div>}
+            <hr/>
+
+
         </div>
     );
 };
