6872cd652941e54bef5ec4172539ef85f9798b96
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-06-04 23:10:14 +0300

    analysis/lesson11/context_structure_dataShare

--- src/components/UserPostsComponent.tsx
+++ src/components/UserPostsComponent.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {FC} from 'react';
+import {IUserWithPostsType} from "../models/IUserWithPostsType";
 
-const UserPostsComponent = () => {
+interface IProps{
+    items: IUserWithPostsType[]
+}
+const UserPostsComponent:FC<IProps> = ({items}) => {
     return (
         <div>
-            UserPostsComponent
+            {
+                items.map(item => <div key={item.id}>
 
+                    {item.name}
+                    <ul>
+                        {item.posts.map((post)=> (<li key={post.id}>{post.title}</li>))}
+                    </ul>
+
+                    </div>)
+            }
         </div>
     );
 };
--- src/pages/UserPostsPage.tsx
+++ src/pages/UserPostsPage.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {UserPostsComponent} from "../components/UserPostsComponent";
+import {useContextProvider} from "../context/ContextProvider";
+import {IUserWithPostsType} from "../models/IUserWithPostsType";
 
 const UserPostsPage = () => {
+
+    const {userStore: {allUsers}, postStore: {allPosts}} = useContextProvider();
+
+    const [userWithPostsState,setUserWithPostsState] = useState<IUserWithPostsType[]>([]);
+
+    const userWithPostsArray = useMemo(() => {
+        return allUsers.map(user => {
+            return {...user, posts: allPosts.filter(post => post.userId === user.id)}
+        })
+    }, [allUsers,allPosts])
+
+    useEffect(() => {
+            setUserWithPostsState(userWithPostsArray)
+            }, [userWithPostsArray]);
+
     return (
         <div>
-            <UserPostsComponent/>
+            <UserPostsComponent items={userWithPostsState}/>
         </div>
     );
 };
