a5fd0cf2f8a3a4ed83dce55afa1ca132372fd43e
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-06-05 19:04:01 +0300

    analysis/lesson11/context_structure_dataShare_as_stateManager

--- src/services/user.api.service.ts
+++ src/services/user.api.service.ts
@@ -1,6 +1,5 @@
 import axios, {AxiosResponse} from "axios";
 import {IUserModel} from "../models/IUserModel";
-import {IPostModel} from "../models/IPostModel";
 
 const axiosInstance = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com',
@@ -12,13 +11,5 @@ const userService = {
         return await axiosInstance.get<IUserModel[]>('/users')
     }
 }
-const postService = {
-    getPosts: async (): Promise<AxiosResponse<IPostModel[]>> => {
-        return await axiosInstance.get<IPostModel[]>('/posts')
-    }
-}
 
-export {
-    userService,
-    postService
-}
+export {userService}
