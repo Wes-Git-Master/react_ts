f8579405643934023a4469825c1c49fcf746a9d2
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-06-08 19:35:57 +0300

    analysis/lesson13/_redux_toolkit_base

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
+import {decrement, increment, incrementsByAmount, useAppDispatch, useAppSelector} from "./redux/store";
 
 
 const App = () => {
+
+    const counter1ValueState = useAppSelector(state => state.counter1SliceState.value)
+    const dispatch = useAppDispatch()
+
     return (
         <div>
-            App
+
+            <h2>{counter1ValueState}</h2>
+            <button onClick={() => dispatch(increment())}> do increment</button>
+            <button onClick={() => dispatch(decrement())}> do decrement</button>
+            <button onClick={() => dispatch(incrementsByAmount(10))}> do inc by amount of 10</button>
+
         </div>
     );
 };
