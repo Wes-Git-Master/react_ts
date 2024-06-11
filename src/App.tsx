47f0412a4a08964a4f5fae0796537255e52b98c0
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-06-11 17:25:09 +0300

    analysis/lesson13/_redux_toolkit_asyncThunk

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
+import {postActions, useAppDispatch, useAppSelector, userActions} from "./redux/store";
 
 const App: FC = () => {
+
+    const dispatch = useAppDispatch();
+
+    const {userSlice: {users}, postSlice: {posts}} = useAppSelector(state => state);
+
+    useEffect(() => {
+        dispatch(userActions.loadUsers())
+        dispatch(postActions.loadPosts())
+    }, []);
     return (
         <div>
-            App
+
+            {
+                users.map(user => <div key={user.id}>{user.id} {user.name}</div>)
+            }
+
+            <hr/>
+
+            {
+                posts.map(post => <div key={post.id}>{post.id} - {post.body} </div>)
+            }
 
         </div>
     );
---
+++ src/redux/store.ts
@@ -0,0 +1,104 @@
+import {IUser} from "../models/IUser";
+import {configureStore, createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {postService, userService} from "../services/api.service";
+import {AxiosError} from "axios";
+import {IPost} from "../models/IPost";
+import {useDispatch, useSelector} from "react-redux";
+
+type UserSliceType = {
+    users: IUser[]
+}
+
+const userInitState: UserSliceType = {
+    users: []
+}
+
+const loadUsers = createAsyncThunk(
+    'userSlice/loadUsers',
+    async (arg, thunkAPI) => {
+
+        try {
+            const users = await userService.getAll();
+            console.log(users)
+            return thunkAPI.fulfillWithValue(users)
+        } catch (e) {
+            const error = e as AxiosError;
+            return thunkAPI.rejectWithValue(error.response?.data)
+        }
+
+    }
+)
+
+/***********************************************************************/
+
+const userSlice = createSlice({
+    name: "userSlice",
+    initialState: userInitState,
+    reducers: {},
+    extraReducers: builder =>
+        builder
+            .addCase(loadUsers.fulfilled, (state, action) => {
+                state.users = action.payload
+            })
+            .addCase(loadUsers.rejected, (state, action) => {
+                //....
+            })
+});
+
+export const userActions = {...userSlice.actions, loadUsers}
+
+/******************************   PostsSlice   *****************************************/
+
+type PostSliceType = {
+    posts: IPost[]
+}
+
+const postsInitState: PostSliceType = {
+    posts: []
+}
+
+const loadPosts = createAsyncThunk(
+    'postSlice/loadPosts',
+    async (arg, thunkAPI) => {
+
+        try {
+            const posts = await postService.getAll();
+            return posts
+        } catch (e) {
+            const error = e as AxiosError;
+            return thunkAPI.rejectWithValue(error.response?.data)
+        }
+
+    }
+)
+
+/***********************************************************************/
+
+const postSlice = createSlice({
+    name: 'postSlice',
+    initialState: postsInitState,
+    reducers: {},
+    extraReducers: builder =>
+        builder
+            .addCase(loadPosts.fulfilled, (state, action) => {
+                state.posts = action.payload
+            })
+            .addCase(loadPosts.rejected, (state, action) => {
+                //...
+            })
+});
+
+export const postActions = {...postSlice.actions, loadPosts}
+
+/**************************/
+
+export const useAppDispatch = useDispatch.withTypes<typeof store.dispatch>();
+export const useAppSelector = useSelector.withTypes<ReturnType<typeof store.getState>>();
+
+
+export const store = configureStore({
+    reducer: {
+        userSlice: userSlice.reducer,
+        postSlice: postSlice.reducer
+    }
+});
