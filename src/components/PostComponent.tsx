69dc17def84ebd36c8949c7793bc15fda20c7c05
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-06-13 15:40:35 +0300

    hw/14_redux_toolkit_asyncThunk

--- src/components/PostComponent.tsx
+++ src/components/PostComponent.tsx
@@ -1,17 +1,24 @@
 import React, {FC} from 'react';
 import {IPost} from "../models/IPost";
+import {useNavigate} from "react-router-dom";
 
 export interface IProps {
     post: IPost
 }
 
 const PostComponent: FC<IProps> = ({post}) => {
+
+    const navigate = useNavigate();
+
     return (
         <div>
             <h4> {post.title} </h4>
             <p> {post.body} </p>
             postId: {post.id}
-            <button> comments </button>
+            <button onClick={() => {
+                navigate(`${post.id}`)
+            }}> comments
+            </button>
         </div>
     );
 };
